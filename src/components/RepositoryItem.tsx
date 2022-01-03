d5cb60f71dda8b1fcb86c63bcceb16ef21b09de8
cesarfavero <cesar.favsouza@gmail.com>
2022-01-02 21:11:22 -0300

    TypeScript Complete Configuration

--- src/components/RepositoryItem.tsx
+++ src/components/RepositoryItem.tsx
@@ -11,7 +11,6 @@ export function RepositoryItem(props: RepositoryItemProps) {
     <li>
       <strong>{props.repository.name}</strong>
       <p>{props.repository.description}</p>
-      <a></a>
 
       <a href={props.repository.html_url}>
         Acessar repositório
